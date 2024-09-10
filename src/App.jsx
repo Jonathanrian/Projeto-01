047e1611552bd2df2a77583d461289853db08f13
jonathanrian <jonathasryann@gmail.com>
2024-09-10 15:07:47 -0300

    commit: "função handleCreateNewComment desenvolvida"

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,38 @@ import './global.css';
 import { Sidebar } from './components/Sidebar';
 
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/95014039?v=4',
+      name: 'Jonathan Ryan',
+      role: 'Web develop'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu GitHub. É um projeto que fiz nos estudos diarios. O nome do projeto é PrimeFlix 🚀',},
+      { type: 'link', content: 'https://prime-five-sable.vercel.app/',},
+    ], 
+    publishedAt: new Date('2024-09-10 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/95014039?v=4',
+      name: 'Samyra Ellen',
+      role: 'Farmaceutica'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu GitHub. É um projeto que fiz nos estudos diarios. O nome do projeto é PrimeFlix 🚀',},
+      { type: 'link', content: 'https://prime-five-sable.vercel.app/',},
+    ], 
+    publishedAt: new Date('2024-09-15 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -18,10 +50,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post 
-            author="Jonathan Ryan"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi distinctio corporis molestias blanditiis harum, nobis dicta nemo esse ea eum fugit similique repudiandae aliquam iste, iusto modi, soluta adipisci praesentium?"
-          />
+          {post.map(post => {
+            return (
+              <Post 
+                author = {post.author}
+                content = {post.content}
+                publishedAt = {post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,63 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post () {
+import { toast } from 'react-toastify';
+import { useState } from 'react';
+
+export function Post ({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publisheDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  function handleCreateNewComment() {
+    event.preventDefault(); //não fazer o redirecionamento de pagina
+
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/95014039?v=4" /> 
+          <Avatar src={author.avatarUrl} /> 
           
             <div className={styles.authorInfo}>
-              <strong>Jonathan Ryan</strong>
-              <span>Web Developer</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
         </div>
 
-        <time title='09 de setembro às 14:30h' dateTime="2024-09-09">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publisheDateRelativeToNow}
         </time>
 
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu GitHub. É um projeto que fiz nos estudos diarios. O nome do projeto é PrimeFlix 🚀</p>
-        <p><a href="https://prime-five-sable.vercel.app/">PrimeFlix</a></p>
-        <p> 
-          <a href="">#NovoProjeto</a> {' '}
-          <a>#Nlw </a> {' '}
-          <a>#Rocketseat</a> {' '}
-        </p>
+        {content.map(line => {
+          if(line.type ==='paragraph'){
+            return <p>{line.content}</p>
+          }else if(line.type === 'link'){
+            return <p> <a href="#">{line.content}</a> </p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={ handleCreateNewComment } className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
@@ -45,9 +70,9 @@ export function Post () {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comments => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
