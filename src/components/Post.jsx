1ffd0ee065edf63a24a8019ae0115da68e0184ca
jonathanrian <jonathasryann@gmail.com>
2024-09-10 23:00:59 -0300

    commit "função de like"

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,8 +34,13 @@ export function Post ({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
 
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
@@ -45,6 +50,8 @@ export function Post ({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne);
     toast.success("Comentário deletado!")
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -82,10 +89,14 @@ export function Post ({ author, publishedAt, content }) {
           placeholder='Deixe seu comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
           
         </footer>
       </form>
